cea04e6c0d21d1d7627f87f302491c53adf57c65
Björn Formgren <formgrenbjorn@gmail.com>
2024-05-18 08:09:58 +0200

    piped the cyclomatic complexity tasks to cc.txt

--- scripts/library_dep_tasks.py
+++ scripts/library_dep_tasks.py
@@ -4,9 +4,11 @@ import json
 with open('../data/ClassEval_data.json') as f:
     data = json.load(f)
 
+
+# with open ('../')
 # Filter tasks with library dependencies
 tasks_with_lib_dependencies = [task for task in data if any(method['dependencies']['lib_dependencies'] for method in task['methods_info'])]
-
-print(f"Number of tasks with at least one method of library dependency: {len(tasks_with_lib_dependencies)}")
+#wizard_with_lib_dependencies = 
+print(f"Number of tasks with at least one method of library dependency in ClassEval.data: {len(tasks_with_lib_dependencies)}")
 
 # Print the names of the tasks with library dependencies 
